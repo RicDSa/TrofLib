29970eaf24a42e3a5f6f90278e0f6c39acfac65e
RicDSA <ricardodanielsa18@gmail.com>
2021-11-09 16:49:14 +0000

    Version 1.0.2
    Fragments

---
+++ app/src/main/java/com/pm/troflib/fragments/book/AddFragment.kt
@@ -0,0 +1,73 @@
+package com.pm.troflib.fragments.book
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.*
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.pm.troflib.R
+import com.pm.troflib.data.entities.Book
+import com.pm.troflib.data.viewmodel.BookViewModel
+import com.pm.troflib.utils.Utils.Companion.hideKeyboard
+import kotlinx.android.synthetic.main.fragment_add.*
+
+class AddFragment : Fragment() {
+
+    private lateinit var mBookViewModel: BookViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        setHasOptionsMenu(true)
+
+        mBookViewModel = ViewModelProvider(this).get(BookViewModel::class.java)
+
+        return view;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_add_book, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        hideKeyboard()
+
+        if (item.itemId == R.id.menu_add) {
+            addProduct()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun addProduct() {
+        if (!isValid()) {
+            return Toast.makeText(
+                requireContext(),
+                getString(R.string.empty_book_name),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        val book = Book(0, productName.text.toString())
+
+        mBookViewModel.addBook(book)
+
+        Toast.makeText(
+            requireContext(),
+            getString(R.string.book_successfully_added),
+            Toast.LENGTH_LONG
+        ).show()
+
+        findNavController().navigate(R.id.action_addFragment_to_listFragment)
+    }
+
+    private fun isValid() : Boolean {
+        return !TextUtils.isEmpty(productName.text.toString())
+    }
+}
---
+++ app/src/main/java/com/pm/troflib/fragments/book/ListFragment.kt
@@ -0,0 +1,45 @@
+package com.pm.troflib.fragments.book
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pm.troflib.R
+import com.pm.troflib.data.viewmodel.BookViewModel
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+class ListFragment : Fragment() {
+
+    private  lateinit var mProductViewModel: BookViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        // Recyclerview
+        val adapter = ListAdapter()
+        val recyclerView = view.recyclerview
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // UserViewModel
+        mBookViewModel = ViewModelProvider(this).get(BookViewModel::class.java)
+        mBookViewModel.readAllBooks.observe(viewLifecycleOwner, Observer { books ->
+            adapter.setData(books)
+        })
+
+        view.btnAddProductFromList.setOnClickListener(){
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view;
+    }
+}
